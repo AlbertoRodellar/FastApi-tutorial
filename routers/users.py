067b3fbec13415f88eb52d2e9093ad26c53d7a95
Alberto Rodellar <arodellar99@gmail.com>
2026-01-29 19:59:56 +0100

    feat: :sparkles: added routes and static files

--- routers/users.py
+++ routers/users.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 
-# Iniciar servidor: uvicorn users:app --reload
-
-app = FastAPI()
+# Prefix para que todas las rutas de aqui sean /users 
+# Tags para ordenar en la documentación
+router = APIRouter(prefix="/users",
+                    tags=["users"],
+                    responses={404: {"message":"No encontrado"}})
 
 # Entidad user
 class User(BaseModel):
@@ -17,33 +19,33 @@ users_list = [User(id=1,name="Beru", surname="Test", url="https://test.com", age
         User(id=2,name="xd", surname="perri", url="https://xd.com", age=23),
         User(id=3,name="Chesi", surname="gato", url="https://gato.com", age=11)]
 
-@app.get("/usersjson")
+@router.get("/usersjson")
 async def users():
     return [{"name": "Beru", "surname": "test", "url": "https://test.com", "age": 35},
             {"name": "XD", "surname": "perro", "url": "https://xd.com", "age": 22},
             {"name": "Chelsi", "surname": "gato", "url": "https://gato.com", "age": 24}]
 
 # Lo devuelve con json, gracias al BaseModel
-@app.get("/users")
+@router.get("/")
 async def users():
     return users_list
 
 # Devuelve usuario por id, con un try except por si no lo encuentra
 
 # Path -> /user/1
-@app.get("/user/{id}")
+@router.get("/{id}")
 async def user(id: int):
     return search_user(id)
 
 
 # Query -> /user/?id=1
-@app.get("/user/")
+@router.get("/")
 async def user(id: int):
     return search_user(id)
 
 
 # Crea nuevo usuario en la array
-@app.post("/user/", response_model=User, status_code=201)
+@router.post("/", response_model=User, status_code=201)
 async def create_user(user: User):
     if type(search_user(user.id)) == User:
         raise HTTPException(status_code=400, detail="Usuario ya existe")
@@ -52,7 +54,7 @@ async def create_user(user: User):
     return user
 
 # Actualiza usuario creado de la array
-@app.put("/user/")
+@router.put("/")
 async def update_user(user: User):
     found = False
     for index, saved_user in enumerate(users_list):
@@ -65,7 +67,7 @@ async def update_user(user: User):
     return user
 
 # Elimina usuario de la array
-@app.delete("/user/{id}")
+@router.delete("/{id}")
 async def delete_user(id: int):
     found = False
     for index, saved_user in enumerate(users_list):
